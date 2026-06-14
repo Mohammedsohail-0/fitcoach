7c094ac1b304501cef09a63268a1fa43839c09a3
mohammedsohail <mohammedsohail01010@gmail.com>
2026-06-15 02:21:04 +0530

    table under construction

--- client/src/pages/CoachDashboard.js
+++ client/src/pages/CoachDashboard.js
@@ -1,10 +1,19 @@
 import React from "react";
+import './CoachDashboard.css';
 import { useState, useEffect } from "react";
 import api from "../services/api";
+import { formToJSON } from "axios";
+
+
+
+
 function CoachDashboard() {
   const [coachName, setCoachName] = useState('');
   const [clients, setClients] = useState([]);
-  const workoutLogs = clients.workoutLogs;
+  
+  clients.map((client)=>{
+    console.log(client.goal);
+  })
 
 
 
@@ -22,37 +31,87 @@ function CoachDashboard() {
   }, [])
 
   //fetch client profiles
-  useEffect(()=>{
-    const fetchClients = async () =>{
-      try{
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
         const clientsObj = await api.get('/coach/clients');
         setClients(clientsObj.data);
-      }catch(error){
+      } catch (error) {
         console.log(error);
       }
     }
     fetchClients();
-  },[])
+  }, [])
 
   //check total logs today
-  function checkLogs(logs){
+  function checkLogs(logs) {
     const today = new Date().toDateString();
-    let hasLoggedToday = logs.some(log => new Date(log.loggedAt).toDateString() === today );
+    let hasLoggedToday = logs.some(log => new Date(log.loggedAt).toDateString() === today);
     return hasLoggedToday
   }
   return (
-    <div className="header">
-      <h1>Hi,{coachName}</h1>
-      <div className="container">
-        <p>Total clients</p>
-        <p className="c-count">{clients.length}</p>
+    <div className="coachDashboard">
+      <div className="header">
+        <h1>Hi, {coachName}</h1>
       </div>
-      <div className="container">
-        <p>Logged today</p>
-        <p className="l-count">{clients.filter(client => checkLogs(client.workoutLogs)).length}</p>
+      <div className="hero">
+        <div className="c-wrapper">
+          <div className="c-container">
+            <p className="count" id="c-count">{clients.length}</p>
+            <p className="c-label">Total clients</p>
+          </div>
+          <div className="c-container">
+            <p className="count" id="l-count">{clients.filter(client => checkLogs(client.workoutLogs)).length}</p>
+            <p className="c-label">Logged today</p>
+          </div>
+        </div>
       </div>
+      {/* Clients table  */}
+
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>CLIENT</th>
+            <th>PROGRAM</th>
+            <th>ACTIVITY LAST 7 DAYS</th>
+            <th>STATUS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            clients.map((client)=>(
+              <tr>
+                <td>
+                  <div className="client-profile-dashboard">
+                    <img className="client-avatar" src="https://i.ibb.co/twxKnHfb/profileavatar.png" alt="client profile"></img>
+                    {client.name}
+                  </div>
+                </td>
+                <td>{client.goal}</td>
+                <td></td>
+                <td></td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+
     </div>
   );
 }
 
 export default CoachDashboard;
+
+
+function ActivityDots(){
+  const[isLogged, setLogged] = useState(false);
+  
+  return(
+    <div className="activity-dots">
+
+
+    </div>
+  );
+}
