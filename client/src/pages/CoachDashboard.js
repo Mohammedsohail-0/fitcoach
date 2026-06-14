bf18760a821de232f6a27b772eb9bc0bd1260316
mohammedsohail <mohammedsohail01010@gmail.com>
2026-06-14 23:34:29 +0530

    Add logged today count to dashboard

--- client/src/pages/CoachDashboard.js
+++ client/src/pages/CoachDashboard.js
@@ -4,7 +4,7 @@ import api from "../services/api";
 function CoachDashboard() {
   const [coachName, setCoachName] = useState('');
   const [clients, setClients] = useState([]);
-
+  const workoutLogs = clients.workoutLogs;
 
 
 
@@ -33,12 +33,23 @@ function CoachDashboard() {
     }
     fetchClients();
   },[])
+
+  //check total logs today
+  function checkLogs(logs){
+    const today = new Date().toDateString();
+    let hasLoggedToday = logs.some(log => new Date(log.loggedAt).toDateString() === today );
+    return hasLoggedToday
+  }
   return (
-    <div>
+    <div className="header">
       <h1>Hi,{coachName}</h1>
-      <div>
-        <h1>Total clients</h1>
-        <p>{clients.length}</p>
+      <div className="container">
+        <p>Total clients</p>
+        <p className="c-count">{clients.length}</p>
+      </div>
+      <div className="container">
+        <p>Logged today</p>
+        <p className="l-count">{clients.filter(client => checkLogs(client.workoutLogs)).length}</p>
       </div>
     </div>
   );
