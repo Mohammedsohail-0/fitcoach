b5e0c4254dc0c52e719a95ce21671284c7859e2c
mohammedsohail <mohammedsohail01010@gmail.com>
2026-07-07 01:02:45 +0530

    create Plan page: workout plan component done

--- client/src/pages/CreatePlan.js
+++ client/src/pages/CreatePlan.js
@@ -1,27 +1,48 @@
 import "./CreatePlan.css";
+import { useParams } from 'react-router-dom';
+import { useState, useEffect, useRef } from "react";
+import api from "../services/api";
+import Button from "../components/Button"
+import { useNavigate } from 'react-router-dom';
+
 
 function CreatePlan() {
+
     return (
-        <>
-        <CreatePlan>
-                <div className="header">
-                    <button className="back-btn">
-                        <svg width="70" height="70" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="miter">
-                            <line x1="19" y1="12" x2="5" y2="12"></line>
-                            <polyline points="12 6 5 12 12 18"></polyline>
-                        </svg>
-                    </button>
-                    <h1>Create Plan</h1>
-                </div>
-                <div>
-                    <label htmlFor="plan-title">Plan Title</label>
-                    <div className="plan-title-container">
-                        <input type="text" className="plan-title" name="plan-title"></input>
-                    </div>
-                </div>
-        </CreatePlan>
-            
-        </>
+        <div className="create-plan">
+            <div className="header">
+                <button className="back-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="60px" fill="#ffffff"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" /></svg>
+                </button>
+                <h1>Create Plan</h1>
+            </div>
+            <WorkoutPlan></WorkoutPlan>
+        </div>
     )
 }
 export default CreatePlan;
+
+export function WorkoutPlan() {
+    const [newPlan, setNewPlan] = useState({
+        title: "",
+        description: ""
+    });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewPlan(prev => ({ ...prev, [name]: value }));
+    };
+    return (
+        <div className="workout-plan">
+            <div className="title-container">
+                <label htmlFor="title"><span>* </span>Plan Title:</label>
+                <input type="text" name="title" onChange={handleChange} placeholder="Untitled Plan..."></input>
+            </div>
+            <textarea
+                name="description"
+                onChange={handleChange}
+                placeholder="description...">
+            </textarea>
+        </div>
+    )
+
+}
